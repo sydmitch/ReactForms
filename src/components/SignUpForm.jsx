792312ce68101f2a964cd9669370f77e264069de
Sydney Mitchell <Sydney@Sydneys-Mac-mini.local>
2025-02-06 12:25:38 -0600

    Completed form and received token

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -7,19 +7,37 @@ export default function SignUpForm() {
 
   async function handleSubmit(event) {
     event.preventDefault();
-    console.log("Hello!");
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          headers: {
+          "Content-type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      );
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      setError(error.message);
+    };
   }
-  
+
   return (
-    <h2>Sign Up!</h2>,
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username: <input value={username} onChange={(e) => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password: <input value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <button>Submit</button>
-    </form>
+    <>
+      <h2>Sign Up!</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username: <input value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Password: <input value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button>Submit</button>
+      </form>
+    </>
   )
 }
